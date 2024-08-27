02d5ee2efc85893c5ddaf34270514914881b4d50
Omkar Jadhav <objadhav28@gmail.com>
2024-08-27 21:55:51 +0530

    Commit

--- hello-world-spring-example/src/main/java/com/example/model/City.java
+++ hello-world-spring-example/src/main/java/com/example/model/City.java
@@ -1,4 +1,18 @@
 package com.example.model;
 
 public class City {
+
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+//    public City(String name) {
+//        this.name = name;
+//    }
 }
