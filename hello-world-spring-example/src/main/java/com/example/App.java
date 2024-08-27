bec28584603d0577b499b2c1c3698f4fca463122
Omkar Jadhav <objadhav28@gmail.com>
2024-08-27 22:28:37 +0530

    Commit

--- hello-world-spring-example/src/main/java/com/example/App.java
+++ hello-world-spring-example/src/main/java/com/example/App.java
@@ -1,13 +1,25 @@
 package com.example;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.example.model.Address;
+import com.example.model.City;
+import com.example.service.HelloService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+//        HelloService helloService = new HelloService();
+//        City city = new City();
+//        city.setName("Pune");
+//        Address address = new Address(city);
+//        helloService.setName("Amit");
+//        helloService.setAddress(address);
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+
+        //HelloService helloService = (HelloService) context.getBean("helloService");
+        HelloService helloService = context.getBean("helloService", HelloService.class);
+
+        helloService.sayHello();
     }
 }
