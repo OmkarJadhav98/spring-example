ed913c0885594960368e5f77918b0df4af73bc0a
Omkar Jadhav <objadhav28@gmail.com>
2024-08-27 21:54:28 +0530

    Commit

--- hello-world-spring-example/src/main/java/com/example/model/Address.java
+++ hello-world-spring-example/src/main/java/com/example/model/Address.java
@@ -1,4 +1,13 @@
 package com.example.model;
 
 public class Address {
+    private City city;
+
+    public Address(City city) {
+        this.city = city;
+    }
+
+    public City getCity() {
+        return city;
+    }
 }
