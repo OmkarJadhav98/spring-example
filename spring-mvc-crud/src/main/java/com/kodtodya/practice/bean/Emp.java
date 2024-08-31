a6c3bf7441e4ec5ddbd2622bc83618a4d25e0ebe
Omkar Jadhav <objadhav28@gmail.com>
2024-08-31 21:25:28 +0530

    Commit

---
+++ spring-mvc-crud/src/main/java/com/kodtodya/practice/bean/Emp.java
@@ -0,0 +1,13 @@
+package com.kodtodya.practice.bean;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Emp {
+    private int id;
+    private String name;
+    private float salary;
+    private String designation;
+}
